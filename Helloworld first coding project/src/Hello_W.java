d7ac441c291dd559a8dd0365067667a1608223a5
APCS3rd <APCS3rd@MHS-Dreyer-14>
2018-08-20 10:43:44 -0700

    First commit to github

---
+++ Helloworld first coding project/src/Hello_W.java
@@ -0,0 +1,13 @@
+/**
+ * Prints string in console
+ * 
+ * @author (Leo Liang) 
+ * @version (8/17/2018)
+ */
+public class Hello_W
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From <Leo Liang>"); //Display the string
+    }
+}
